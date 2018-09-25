98c122041161807e5608cd2bb44ec944b4394512
guhao022 <378999587@qq.com>
2018-09-25 12:55:21 +0800

    fix

--- config/config.php
+++ config/config.php
@@ -6,12 +6,14 @@ return [
     ],
 
     "handler" => [
-        Weasy\Proxy\ApiController::class,
         Weasy\User\Api\AuthApi::class,
         Weasy\User\Api\UserApi::class,
+        Weasy\Proxy\ApiController::class,
 
         // 商城接口
         Weasy\Mall\Controllers\Api\NavBarApi::class,
+        Weasy\Mall\Controllers\Api\GoodsApi::class,
+        Weasy\Mall\Controllers\Api\CartApi::class,
 
         // 内容接口
         //Weasy\Stcms\Controllers\ApiController::class,
